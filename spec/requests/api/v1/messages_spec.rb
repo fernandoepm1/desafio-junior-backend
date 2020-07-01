07f194fc28c0bfb14c2da5528ca4f7aa81e71f98
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-06-30 23:01:24 -0300

    Create messages request spec with pending examples

---
+++ spec/requests/api/v1/messages_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe 'Messages API Requests', type: :request do
+  describe 'GET #index' do
+    context 'without authorization header' do
+      #pending
+    end
+
+    context 'with authorization header' do
+      #pending
+    end
+  end
+
+  describe 'POST #create' do
+    context 'without authorization header' do
+      #pending
+    end
+
+    context 'with authorization header' do
+      #pending
+    end
+  end
+
+  describe 'GET #show' do
+    context 'without authorization header' do
+      #pending
+    end
+
+    context 'with authorization header' do
+      #pending
+    end
+  end
+
+  describe 'GET #sent' do
+    context 'without authorization header' do
+      #pending
+    end
+
+    context 'with authorization header' do
+      #pending
+    end
+  end
+end
