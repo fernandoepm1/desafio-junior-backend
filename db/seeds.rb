bba6ff0b3e5638c07c3ae6e59fe66732871f87cf
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-06-30 21:22:23 -0300

    Update seeds with permissions and new users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'Admin',email: 'master@email.com', password: '123456', password_confirmation: '123456')
+# Admin
+User.create(name: 'Admin', email: 'master@email.com', password: '123456', password_confirmation: '123456', permission: :master)
+
+# Normal Users
+User.create(name: 'Mario', email: 'mario@email.com', password: '123456', password_confirmation: '123456', permission: :normal)
+User.create(name: 'Luigi', email: 'luigi@email.com', password: '123456', password_confirmation: '123456', permission: :normal)
+User.create(name: 'Peach', email: 'peach@email.com', password: '123456', password_confirmation: '123456', permission: :normal)
+User.create(name: 'Toad', email: 'toad@email.com', password: '123456', password_confirmation: '123456', permission: :normal)
+User.create(name: 'Wario', email: 'wario@email.com', password: '123456', password_confirmation: '123456', permission: :normal)
+User.create(name: 'Bowser', email: 'bowser@email.com', password: '123456', password_confirmation: '123456', permission: :normal)
