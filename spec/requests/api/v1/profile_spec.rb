4669ece9f88ca5a236681f6a6e5810e86630eebf
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-06-30 23:01:34 -0300

    Create profile request spec with pending examples

---
+++ spec/requests/api/v1/profile_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'Profile API Requests', type: :request do
+  describe 'PUT #update' do
+    context 'without authorization header' do
+      #pending
+    end
+
+    context 'with authorization header' do
+      #pending
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'without authorization header' do
+      #pending
+    end
+
+    context 'with authorization header' do
+      #pending
+    end
+  end
+
+  describe 'GET #show' do
+    context 'without authorization header' do
+      #pending
+    end
+
+    context 'with authorization header' do
+      #pending
+    end
+  end
+end
