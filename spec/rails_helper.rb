cb62b4aace09a44b34746c8b83ea58d707ea8311
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-07-02 00:25:47 -0300

    Update JSON parser helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,7 @@ RSpec.configure do |config|
   Capybara.javascript_driver = :poltergeist
 
   config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Request::Helpers, type: :request
+  config.include RequestSpecHelper, type: :request
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
